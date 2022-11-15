04ba0dee5d8b1d49c48441b136bf78ee3b15d962
TopVirusMinus <mwmma5000@gmail.com>
2022-11-15 17:09:38 +0200

    added arrows and getters

--- first-project/basic.py
+++ first-project/basic.py
@@ -8,9 +8,17 @@ class Test(Scene):
         tri = Triangle().scale(0.6).to_edge(DR)
         
         self.play(Write(title))
-        self.play(DrawBorderThenFill(sq), run_time=3)
-        self.play(Create(circ), run_time=3)
-        self.play(Create(tri), run_time=3)
+        self.play(DrawBorderThenFill(sq),Create(circ),Create(tri), run_time=1.5)
+        self.wait()
+        self.play(title.animate.to_edge(UR),sq.animate.scale(2),sq.animate.rotate(90), tri.animate.to_edge(DL), run_time=1.5)
         self.wait()
         
-        self.play(name.animate.to_edge(UR))
+class Arrows(Scene):
+    def construct(self):
+        rect = Rectangle(color=WHITE, height=3, width = 3).to_edge(UL).scale(0.5)
+        circ = Circle(radius=2).scale(0.5).to_edge(DOWN)
+        arrow = always_redraw(lambda: Line(start=rect.get_bottom(), end=circ.get_top(), buff=0.1).add_tip())
+        
+        self.play(Create(VGroup(rect, circ,arrow)))
+        self.wait()
+        self.play(rect.animate.to_edge(UR), run_time=1.5)
